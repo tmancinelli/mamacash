900a1bbda76de3cc1a28047467302f6af8054e7e
Tiziana Mancinelli <tiziana.mancinelli@gmail.com>
2021-03-06 11:11:12 +0100

    the rest of the exercises

--- exercise1/exercise1.js
+++ exercise1/exercise1.js
@@ -1,12 +1,10 @@
 //This is the first exercise written in Javascript
-
-var text = "It is a long established fact that a reader \nwill be distracted by the readable content \nof a page when looking at its layout. \nThe point of using Lorem Ipsum is \nthat it has a more-or-less normal \ndistribution of letters.";
-
-//
+var text = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.";
+//splitting the strings in break lines
 const para = text.split("\n");
-//
+//reverse the lines
 const reversed = para.reverse();
-//
+//from an array to string
 const joined = reversed.join('\n');
 console.log(joined);
 
